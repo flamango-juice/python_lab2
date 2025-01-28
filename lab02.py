62586ea257b3afbd9d0477d45eb39300b739d904
flamango-juice <ethanalburo0221@gmail.com>
2025-01-27 17:00:48 -0800

    more updates

--- lab02.py
+++ lab02.py
@@ -4,7 +4,7 @@ import math
 
 # 1.1
 name = "John Doe"
-age = 200
+age = 67
 height = 5 + (7/12)
 favourite_colour = "Green"
 
@@ -17,7 +17,7 @@ print(favourite_colour)
 print(name, age, height, favourite_colour)
 
 print(f"Hello! {favourite_colour} is my favourite colour!")
-print(f"I am {height:2f} ft tall.")
+print(f"I am {height:.2f} ft tall.")
 
 print(f"""
     Name: {name}
@@ -27,23 +27,44 @@ print(f"""
 """)
 
 #1.3
-rad = int(input("Enter a value:"))
+
+rad = 5
 circle_area = math.pi * (rad ** 2)
 print(round(circle_area,1))
-print(f"The area of a circle with the radius of {rad} is {circle_area}!")
+print(f"The area of a circle with the radius of {rad} is {circle_area:.1f}!")
 
 # Part 2: Statements and Modules
-print(math.sqrt(age))
+# 2.2
+print(f"The square root of my age is {math.sqrt(age):.2f}")
 
-sin = math.sin(height)
-cos = math.cos(height)
-print(sin, cos)
+# 2.3
+print(f"""
+    The sine of {height:.2f}: {math.sin(height):.2f}
+    The cosine of {height:.2f}: {math.cos(height):.2f}
+""")
 
 # Part 3: Expressions and Operators
-a = age + 5
-b = height - 4
-c = age * height
-d = height / 2
-e = (height / 3) - (height // 3)
-f = age**2
-print(a,b,c,d,e,f)
+a = age + 5 # The sum of age and 5.
+b = height - 4 # The difference between height and 4.
+c = age * height # The product of age and height.
+d = height / 2 # The quotient of height and 2.
+e = height % 3 # The remainder of age divided by 3.
+f = age ** 2 # age raised to the power of 2.
+print(f"""
+    {a:d}
+    {b:.2f}
+    {c:.2f}
+    {d:.2f}
+    {e:.2f}
+    {f:d}
+""")
+
+# Part 4: Temperature Conversion
+fahrenheit = int(input("Enter the current temperature in Fahrenheit:"))
+celsius = (fahrenheit - 32) * 5/9
+print(f"""
+Celsius: {celsius:.0f}°C
+Fahrenheit: {fahrenheit}°F
+""")
+
+
