ff44268297dc65ba885533d2dfc0184831cfe692
flamango-juice <ethanalburo0221@gmail.com>
2025-01-27 16:41:24 -0800

    more updates

--- lab02.py
+++ lab02.py
@@ -20,17 +20,17 @@ print(f"Hello! {favourite_colour} is my favourite colour!")
 print(f"I am {height:2f} ft tall.")
 
 print(f"""
-Name: {name}
-Age: {age}
-Height: {height}
-Favorite color: {favourite_colour}
+    Name: {name}
+    Age: {age}
+    Height: {height}
+    Favorite color: {favourite_colour}
 """)
 
 #1.3
-rad = 5
+rad = int(input("Enter a value:"))
 circle_area = math.pi * (rad ** 2)
 print(round(circle_area,1))
-
+print(f"The area of a circle with the radius of {rad} is {circle_area}!")
 
 # Part 2: Statements and Modules
 print(math.sqrt(age))
@@ -44,5 +44,6 @@ a = age + 5
 b = height - 4
 c = age * height
 d = height / 2
-e = height // 3
-
+e = (height / 3) - (height // 3)
+f = age**2
+print(a,b,c,d,e,f)
